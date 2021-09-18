94ba2dbd1867d0455d290ad1392ea64c55e10de6
itsmeny <68806699+itsmeny@users.noreply.github.com>
2021-09-18 23:54:35 +0700

    Update WS01_63130500024.R

--- assignment/WS01_63130500024/WS01_63130500024.R
+++ assignment/WS01_63130500024/WS01_63130500024.R
@@ -1,44 +1,52 @@
-# Student_ID
-# Step 0
+#63130500024
+
+# Step 1: Define a question
+## 1.How many observation of this dataset (before cleaning) ?
+## 2.Are there duplicate data sets ? (If have duplicate data, list the data that duplicate)
+## 3.How many distinct school in this dataset ? (Know after drop duplicate data)
+## 4.What is min, max, average, quartile of each part in SAT ?
+## 5.What is min, max, average, quartile of total score in SAT ?
+## 6.Which school is get highest SAT score ?
+
+
+# Step 2: Loading library and dataset  
+## Library
+library(stringr)
+library(readr)
+library(assertive)
 library(dplyr)
+## Dataset
+sat_score <- read_csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/SAT_original.csv")
 
-# Explore
+# Step 3: Cleaning Data
+## Remove duplicate data
+sat_score <- sat_score %>% distinct()
+## Replace 's' with NA (Handle Missing Values)
+sat_score$math_score <- replace(sat_score$math_score,sat_score$math_score == 's',NA)
+sat_score$reading_score <- replace(sat_score$reading_score,sat_score$reading_score == 's',NA)
+sat_score$writing_score <- replace(sat_score$writing_score,sat_score$writing_score == 's',NA)
+sat_score <- sat_score %>% na.omit()
+## Change score datatype
+sat_score$math_score <- sat_score$math_score %>% as.numeric()
+sat_score$reading_score <- sat_score$reading_score %>% as.numeric()
+sat_score$writing_score <- sat_score$writing_score %>% as.numeric()
 
-# xxxx
+# Question
+## 1. How many observation of this dataset (before cleaning) ?
+glimpse(sat_score)
 
+## 2. Are there duplicate data sets ? (If have duplicate data, list the data that duplicate)
+sat_score %>% filter(duplicated(sat_score))
 
+## 3. How many distinct school in this dataset ? (Know after drop duplicate data)
+sat_score %>% distinct() %>% count()
 
-# USEFUL FUNCTION (After finished please remove it) -------------------------
-## Stat
-sum()
-min()
-mean()
-max()
+## 4. What is min, max, average, quartile of each part in SAT ?
+sat_score %>% select(math_score,reading_score,writing_score) %>% summary()
 
-## Explore
-head()      # See 6 observation (rows)
-str()       # Structure of object
-summary()
+## 5. What is min, max, average, quartile of total score in SAT ?
+sat_score <- sat_score %>% mutate(total_score = math_score+reading_score+writing_score)
+sat_score %>% select(total_score) %>% summary()
 
-## dplyr package
-glimpse()
-select()
-filter()
-arrange()
-mutate()
-group_by()
-summarise()
-count()
-
-## Cleaning
-as.vector()
-as.numeric()
-na.omit()
-is.na()
-replace()
-distinct()
-duplicated()
-
-#assertive package
-assert_is_numeric()
-assert_all_are_in_closed_range()
+## 6. Which school is get highest SAT score ?
+sat_score %>% arrange(desc(total_score)) %>% head(n = 1)
