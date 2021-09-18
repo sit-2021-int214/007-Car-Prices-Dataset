40b253f0b4a75e455cc3eb445db23f194b51abf2
Nitipoom Suttachai <63338342+Nitipoom1456@users.noreply.github.com>
2021-09-19 01:05:00 +0700

    update .r

--- assignment/WS01_63130500071/WS01_63130500071.R
+++ assignment/WS01_63130500071/WS01_63130500071.R
@@ -1,4 +1,5 @@
 # 63130500071
+#Step 0
 library(dplyr)
 library(readr)
 library(stringr)
@@ -7,21 +8,21 @@ library(assertive)
 sat_score <- read_csv('https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/SAT_original.csv')
 View(sat_score)
 
+#Step 1
 #1
 glimpse(sat_score)
 
-assert_is_a_number(sat_score$math_score)
-is.numeric(sat_score$math_score)
 
-#change type
+#Step 2 change type
 sat_score$math_score <- as.numeric(sat_score$math_score)
 sat_score$reading_score <- as.numeric(sat_score$reading_score)
 sat_score$writing_score <- as.numeric(sat_score$writing_score)
 
+#Step 3
 #2
-sat_score$DBN %>% duplicated() %>% sum()
-sat_score$DBN %>% duplicated() %>% View()
+sat_score %>% filter(duplicated(sat_score))
 
+#Step 4
 #cleaning data
 sat_score <- sat_score %>% distinct()
 sat_score$math_score <- replace(sat_score$math_score,sat_score$math_score == 's',NA)
@@ -29,6 +30,7 @@ sat_score$reading_score <- replace(sat_score$reading_score,sat_score$reading_sco
 sat_score$writing_score <- replace(sat_score$writing_score,sat_score$writing_score == 's',NA)
 sat_score <- sat_score %>% na.omit()
 
+#Step 5
 #handling out of range
 sat_score %>% filter(math_score < 200 | reading_score < 200 | writing_score < 200)
 sat_score %>% filter(math_score > 800 | reading_score > 800 | writing_score > 800)
@@ -48,10 +50,12 @@ sat_score %>% distinct() %>% count()
 sat_score <- sat_score %>% mutate(total_score = math_score+reading_score+writing_score) 
 
 #4
-sat_score %>% select(math_score,reading_score,writing_score) %>% summary()
+sat_score %>% summary()
 
 #5
 sat_score %>% select(total_score) %>% summary()
 
 #6
 sat_score %>% arrange(desc(total_score)) %>% head(n = 1L)
+
+write_csv(sat_score,file = "SAT_clean.csv")
