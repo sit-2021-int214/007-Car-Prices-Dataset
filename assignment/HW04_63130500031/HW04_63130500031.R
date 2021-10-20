566d497d29ab3e61f6232ccca0de13382f817c10
nortorkor <immintchyy@gmail.com>
2021-10-20 21:56:24 +0700

    ''

---
+++ assignment/HW04_63130500031/HW04_63130500031.R
@@ -0,0 +1,8 @@
+#Load Library
+library(stringr)
+library(readr)
+library(assertive)
+library(dplyr)
+
+# Load data
+prog_book <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/prog_book.csv")
